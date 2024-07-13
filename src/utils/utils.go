72744a34f1826ef81abe4b53ff9f11969924bb26
oscarquin27 <oscar.272731@gmail.com>
2024-07-13 16:52:09 -0400

    added services

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"math/big"
 	"strconv"
 	"time"
@@ -18,6 +19,26 @@ func ParseInt(s string) int {
 
 func ConvertToPgTypeInt4(i int) pgtype.Int4 {
 	return pgtype.Int4{Int32: int32(i), Valid: true}
+}
+
+func ConvertFromInt4Array(i []pgtype.Int4) []string {
+	var s []string
+
+	for _, n := range(i) {
+		s = append(s, ConvertFromInt4(n))
+	}
+
+	return s
+}
+
+func ConvertToInt4Array(i []string) []pgtype.Int4 {
+	var s []pgtype.Int4
+
+	for _, n := range(i) {
+		s = append(s, ConvertToPgTypeInt4(ParseInt(n)))
+	}
+
+	return s
 }
 
 func ConvertFromInt4(i pgtype.Int4) string {
@@ -49,7 +70,27 @@ func ConvertToPgTypeDate(d string) pgtype.Date {
 }
 
 func ConvertFromDate(d pgtype.Date) string {
+	d.Valid = true
 	return d.Time.String()
+}
+
+func ConvertFromDateTime(d pgtype.Time) string {
+	hours := d.Microseconds / int64(time.Hour) / 1000
+    minutes := (d.Microseconds - hours * int64(time.Hour) * 1000) / int64(time.Minute)
+    seconds := (d.Microseconds - hours * int64(time.Hour) * 1000 - minutes * int64(time.Minute)) / int64(time.Second)
+  
+  	return fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)	
+}
+
+func ConvertToPgTypeDateTime(d string) pgtype.Time {
+	
+	t, err := time.Parse("2024-01-01 10:00:00 00.000", d)
+
+	if err != nil {
+		t = time.Now().UTC()
+	}
+  
+  	return pgtype.Time{Microseconds: t.Unix(), Valid: true}	
 }
 
 func ConvertToPgTypeBool(b bool) pgtype.Bool {
