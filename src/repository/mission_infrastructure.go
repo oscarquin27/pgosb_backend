81c8e4e930f6aeaa7e8331e68d96adc2867fda93
oscarquin27 <oscar.272731@gmail.com>
2024-08-04 12:37:32 -0400

    mikroimpl

--- src/repository/mission_infrastructure.go
+++ src/repository/mission_infrastructure.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"fdms/src/mikro"
 	"fdms/src/models"
 	"fdms/src/services"
 
@@ -65,38 +66,15 @@ FROM missions.infrastructure
 }
 
 func (u *MissionInfrastructureRepository) Create(infra *models.MissionInfrastructure) error {
-	ctx := context.Background()
+	m := mikro.NewMkModel(u.db)
 
-	conn, err := u.db.Acquire(ctx)
-	defer conn.Release()
-
-	if err != nil {
-		return err
-	}
-
-	rows, err := conn.Exec(ctx, `
-	insert into missions.infrastructure
-	(service_id, build_type, build_occupation, build_area, build_access, levels, people, goods_type, build_roof, build_floor, build_room_type, observations)
-	values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);
-;
-`, infra.ServiceId,
-   infra.BuildType,
-   infra.BuildOccupation,
-   infra.BuildArea,
-   infra.BuildAccess,
-   infra.Levels,
-   infra.People,
-   infra.GoodsType,
-   infra.BuildRoof,
-   infra.BuildFloor,
-   infra.BuildRoomType,
-   infra.Observations)
+	rows, err := m.Model(infra).Omit("id").Insert("missions.infrastructure")
 
 	if err != nil {
 		return err
 	}
 
-	if rows.RowsAffected() > 0 {
+	if rows > 0 {
 		return nil
 	}
 
@@ -104,50 +82,15 @@ func (u *MissionInfrastructureRepository) Create(infra *models.MissionInfrastruc
 }
 
 func (u *MissionInfrastructureRepository) Update(infra *models.MissionInfrastructure) error {
-	ctx := context.Background()
+	m := mikro.NewMkModel(u.db)
 
-	conn, err := u.db.Acquire(ctx)
-	defer conn.Release()
-
-	if err != nil {
-		return err
-	}
-
-	rows, err := conn.Exec(ctx, `
-		UPDATE missions.infrastructure
-		SET service_id=$1, 
-		build_type=$2, 
-		build_occupation=$3, 
-		build_area=$4, 
-		build_access=$5, 
-		levels=$6, 
-		people=$7, 
-		goods_type=$8, 
-		build_roof=$9, 
-		build_floor=$10, 
-		build_room_type=$11, 
-		observations=$12
-		where id = $13;
-		`,
-   		infra.ServiceId,
-   		infra.BuildType,
-   		infra.BuildOccupation,
-   		infra.BuildArea,
-   		infra.BuildAccess,
-   		infra.Levels,
-   		infra.People,
-   		infra.GoodsType,
-   		infra.BuildRoof,
-   		infra.BuildFloor,
-   		infra.BuildRoomType,
-   		infra.Observations,
-		infra.Id)
+	rows, err := m.Model(infra).Omit("id").Where("id", "=", infra.Id).Update("missions.infrastructure")
 
 	if err != nil {
 		return err
 	}
 
-	if rows.RowsAffected() > 0 {
+	if rows > 0 {
 		return nil
 	}
 
