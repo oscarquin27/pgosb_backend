acf16c9a89e091870aeff3a08a9d4494c410ba1c
Orlando Torres R <orlandoenriqueiii@gmail.com>
2024-08-05 17:37:32 -0400

    last fixeds

--- src/mikro/mikroimpl.go
+++ src/mikro/mikroimpl.go
@@ -122,7 +122,7 @@ func executeSentenceReturning(pg *pgxpool.Pool, sql string, values []interface{}
 		return err
 	}
 
-	defer conn.Conn().Close(ctx)
+	defer conn.Release()
 
 	err = conn.QueryRow(ctx, sql, values...).Scan(&model.model)
 
@@ -133,27 +133,6 @@ func executeSentenceReturning(pg *pgxpool.Pool, sql string, values []interface{}
 	return nil
 
 }
-
-// func executeSelectRows(pg *pgxpool.Pool, sql string, values []interface{}, model interface{}) (any, error) {
-// 	ctx := context.Background()
-
-// 	conn, err := pg.Acquire(ctx)
-
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	defer conn.Conn().Close(ctx)
-
-// 	rows, err := conn.Query(ctx, sql, values...)
-
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	r, err := pgx.CollectRows(rows, pgx.RowToStructByName[model])
-
-// }
 
 // Omite el campo a ser actualizado
 func (mk *MkModel) Omit(field string) *MkModel {
