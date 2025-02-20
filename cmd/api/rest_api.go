ae5779c3391d63c00bb8a1f656f6dbb687b8d3d9
Orlando Torres R <orlandoenriqueiii@gmail.com>
2025-02-20 17:16:17 -0400

    ultimos cambios

--- cmd/api/rest_api.go
+++ cmd/api/rest_api.go
@@ -155,7 +155,7 @@ func Run(db *pgxpool.Pool, auth *keycloak.KeycloakAuthenticationService) {
 
 	conf.AllowCredentials = true
 
-	conf.AllowOrigins = []string{"https://gres.local.net:8083", "http://localhost:5173", "http://localhost:5174",
+	conf.AllowOrigins = []string{"https://gres.local.net:8083", "http://localhost:5173", "http://localhost:5175",
 		"http://192.168.120.136:5173", "http://192.168.100.108:5173", "https://hackorlandodev.com:8083",
 		"http://pruebas.gres.net:8083", "http://192.168.100.103:5173", "http://192.168.120.145:5173"}
 
